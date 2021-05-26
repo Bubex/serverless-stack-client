bdd87d9eddfa2032d3bf895a4955bab286f37a22
Bubex <marlon2mrt@gmail.com>
2021-05-26 01:46:10 -0300

    Adding a Netlify build script

--- src/config.js
+++ src/config.js
@@ -1,20 +1,44 @@
-const config = {
-    STRIPE_KEY: "pk_test_51Iv9cfDZcGhTGqWXzJ8cWLYbRHKEKtGWdV9lnkd5lTgNkp6rOEFbWREBjO48Eiaka0YLaRh9FMyIHuuwk9Br2Nv2009OTvEXnk",
-    MAX_ATTACHMENT_SIZE: 5000000,
-    s3: {
-      REGION: "us-east-1",
-      BUCKET: "marlonf-notes-app-upload",
-    },
-    apiGateway: {
-      REGION: "us-east-1",
-      URL: "https://0f94aey7b8.execute-api.us-east-1.amazonaws.com/prod",
-    },
-    cognito: {
-      REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_KAhxgfPdd",
-      APP_CLIENT_ID: "7holoieql8dj4tneitb4ous85",
-      IDENTITY_POOL_ID: "us-east-1:f7e40c97-84d8-4a8e-8d15-c8af441208db",
-    },
+const dev = {
+  STRIPE_KEY: "pk_test_51Iv9cfDZcGhTGqWXzJ8cWLYbRHKEKtGWdV9lnkd5lTgNkp6rOEFbWREBjO48Eiaka0YLaRh9FMyIHuuwk9Br2Nv2009OTvEXnk",
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "dev-notes-infra-s3-uploads4f6eb0fd-trut93qxmuu6"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://2440oqd981.execute-api.us-east-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_bIkRInEva",
+    APP_CLIENT_ID: "5il7434em12pcib3vvigtb5h96",
+    IDENTITY_POOL_ID: "us-east-1:7ed28d64-6b7e-494d-9cce-ea733b4de22a"
+  }
 };
-  
+
+const prod = {
+  STRIPE_KEY: "pk_test_51Iv9cfDZcGhTGqWXzJ8cWLYbRHKEKtGWdV9lnkd5lTgNkp6rOEFbWREBjO48Eiaka0YLaRh9FMyIHuuwk9Br2Nv2009OTvEXnk",
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "prod-notes-infra-s3-uploads4f6eb0fd-1euskruzmji25"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://0f94aey7b8.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_FcKwrjky4",
+    APP_CLIENT_ID: "35otjbfsc07jrtuhe4j6ohjd7l",
+    IDENTITY_POOL_ID: "us-east-1:04f3c22d-6a76-46fa-bed3-750caa0a92c2"
+  }
+};
+
+const config = {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  // Default to dev if not set
+  ...(process.env.REACT_APP_STAGE === "prod" ? prod : dev),
+};
+
 export default config;
